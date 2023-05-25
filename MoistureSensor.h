dd991a83585b9898507182ea4317f2f6646cab12
Prateek Sahay <ptkinvent@gmail.com>
2023-05-25 11:25:53 -0700

    Inverted signal from moisture sensor for consistency

--- MoistureSensor.h
+++ MoistureSensor.h
@@ -13,4 +13,14 @@ public:
      * Plug moisture sensor into an analog pin of Grove shield.
      */
     MoistureSensor(int pin) : AbstractAnalogSensor(pin) {};
+
+    /**
+     * Returns sensed value 0-1
+     */
+    virtual float sense()
+    {
+        // Invert the signal since this is more intuitive. E.g. 0 is low
+        // moisture and 1 is high moisture.
+        return 1 - AbstractAnalogSensor::sense();
+    };
 };
